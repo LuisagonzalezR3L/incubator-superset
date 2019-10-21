9b361111f4bd871cdb7b41630894847c57978931
Luisa Gonzalez <luisa.gonzalez@rokk3rlabs.com>
2019-10-21 17:10:41 -0500

    Get user

--- contrib/docker/custom_sso_security_manager.py
+++ contrib/docker/custom_sso_security_manager.py
@@ -11,12 +11,10 @@ from flask_appbuilder.security.views import UserDBModelView,AuthDBView
 from superset.security import SupersetSecurityManager
 from flask_appbuilder.security.views import expose
 from flask_appbuilder.security.manager import BaseSecurityManager
-from flask_login import login_user, logout_user
-
-
+from flask_login import login_user, logout_user, LoginManager, UserMixin
+import logging
 class CustomAuthDBView(AuthDBView):
     login_template = 'appbuilder/general/security/login_db.html'
-
     @expose('/login/', methods=['GET', 'POST'])
     def login(self):
         redirect_url = self.appbuilder.get_url_for_index
@@ -27,12 +25,19 @@ class CustomAuthDBView(AuthDBView):
             user = self.appbuilder.sm.find_user(username=request.args.get('username'))
             login_user(user, remember=False)
             return redirect(redirect_url)
-        
         else:
-            flash('Unable to auto login', 'warning')
             return super(CustomAuthDBView,self).login()
+
+class User(UserMixin):
+    def __init__(self, user):
+        self.user = user
+        self.roles = [{'name': 'Admin', 'id': 1}]
 
 class CustomSecurityManager(SupersetSecurityManager):
     authdbview = CustomAuthDBView
+    def load_user_from_request(self, header_val):  
+        user = self.appbuilder.sm.find_user(username=header_val)
+        return User({'username': 'Luisa' })
     def __init__(self, appbuilder):
         super(CustomSecurityManager, self).__init__(appbuilder)
+        self.lm.header_loader(self.load_user_from_request)
